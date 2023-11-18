2fa561417e2189001c71097f65b2defe84f91de6
meiyuouo <meiyu19980928@gmail.com>
2023-11-19 01:03:22 +0800

    refactor
    - move some variable from local to global
    - fix using of magic number
    - remove unnecessary variable
    - rename some variable to be more readable

--- src/main/java/com/marykuo/runningcookie/Sound.java
+++ src/main/java/com/marykuo/runningcookie/Sound.java
@@ -5,31 +5,30 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class Sound {
-    public static synchronized void play(int fileName) 
-    {
-    	final int MUSIC_N=7;
-    	final int BACK_GROUNG=0;
-    	final int JUMP=1;
-    	final int JUMP2=2;
-    	final int FALLING=3;
-    	final int EATCAKE=4;
-    	final int UP_HEARTVALUE=5;
-    	final int HIT=6;
+    public final static int MUSIC_N = 7;
+    public final static int BACKGROUND = 0;
+    public final static int PLAYER_JUMP = 1;
+    public final static int PLAYER_JUMP2 = 2;
+    public final static int PLAYER_FALLING = 3;
+    public final static int EAT_CAKE = 4;
+    public final static int EAT_HEART_POTION = 5;
+    public final static int PLAYER_HIT = 6;
 
-    	String[] address;
-    	address = new String[MUSIC_N];
-    	address[BACK_GROUNG]="./image/audio/background.wav";
-    	address[JUMP]="./image/audio/jump.wav";
-    	address[JUMP2]="./image/audio/jump2.wav";
-    	address[FALLING]="./image/audio/falling.wav";
-    	address[EATCAKE]="./image/audio/eatcake.wav";
-    	address[UP_HEARTVALUE]="./image/audio/up_heartvalue.wav";
-    	address[HIT]="./image/audio/hit.wav";
-    	
-        new Thread(new Runnable() { 
-            public void run() {//AudioSystem.getAudioInputStream(new File(address[fileName]));
+    public static synchronized void play(int fileName) {
+        String[] address = new String[MUSIC_N];
+        address[BACKGROUND] = "./image/audio/background.wav";
+        address[PLAYER_JUMP] = "./image/audio/jump.wav";
+        address[PLAYER_JUMP2] = "./image/audio/jump2.wav";
+        address[PLAYER_FALLING] = "./image/audio/falling.wav";
+        address[EAT_CAKE] = "./image/audio/eatcake.wav";
+        address[EAT_HEART_POTION] = "./image/audio/up_heartvalue.wav";
+        address[PLAYER_HIT] = "./image/audio/hit.wav";
+
+        new Thread(new Runnable() {
+            public void run() {
                 try {
                     Clip clip = AudioSystem.getClip();
+                    // AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(address[fileNam]));
                     AudioInputStream inputStream = AudioSystem.getAudioInputStream(this.getClass().getResourceAsStream(address[fileName]));
                     clip.open(inputStream);
                     clip.start();
--- src/main/java/com/marykuo/runningcookie/WalkMian.java
+++ src/main/java/com/marykuo/runningcookie/WalkMian.java
@@ -16,24 +16,21 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static com.marykuo.runningcookie.Sound.*;
+
 public class WalkMian extends JFrame implements KeyListener {
-	Image img,img2,img3,img4;
-	Image jump,jump2,jump3,jump4;
-	Image slide,slide2;
-	Image[] images = {img,img2,img3,img4};
-    Image[] slides = {slide,slide2,slide,slide2};
-    Image[] jumps = {jump,jump2,jump3,jump4};
-	Image back;
-	Image floor;
-    Image[] floors = {floor,floor,floor,floor,floor,floor,floor,floor,floor,floor,floor};
-	Image obstacle1;
-	Image obstacle2;
-	Image obstacle3;
-	Image fork;
-	Image cake;
-	Image heart;
-	Image heart_eat;
-	Image heart_long;
+	private final Image backGround = getToolkit().getImage("./image/background.png");
+	private final Image  cake = getToolkit().getImage("./image/cake.png");
+	private final Image heart = getToolkit().getImage("./image/heart.png");
+	private final Image heart_eat = getToolkit().getImage("./image/heart_eat.png");
+	private final Image heart_long = getToolkit().getImage("./image/heart_long.png");
+
+	Image[] player_run_images = new Image[4];
+    Image[] player_slide_images = new Image[4];
+    Image[] player_jump_images = new Image[4];
+    Image[] floors = new Image[11];
+	Image[] obstacles = new Image[4];
+
 	Font score_font;
 	int heart_value=300;
 	int score=0;
@@ -49,66 +46,50 @@ public class WalkMian extends JFrame implements KeyListener {
 	public boolean key_slide = false;
 	public boolean key_jump = false;
 	public boolean key_jump2 = false;
-	
+
 	public boolean running = true;
-	
+
 	public int key_jump2_y=0;
 	int h=100,w=100;
 	int x = 120, y = 500, dir = 0, num = 0;
 	int i = 0;
-	
+
 	// Constructor
 	public WalkMian() throws FileNotFoundException {
 		super("Running cookie");
-		//���ۿo�B�����
-		img = getToolkit().getImage("./image/run-0.png");
-		img2 = getToolkit().getImage("./image/run-1.png");
-		img3 = getToolkit().getImage("./image/run-2.png");
-		img4 = getToolkit().getImage("./image/run-3.png");
-		images[0]=img;
-		images[1]=img2;
-		images[2]=img3;
-		images[3]=img4;
-		
-		slide = getToolkit().getImage("./image/slide-2.png");
-		slide2 = getToolkit().getImage("./image/slide-3.png");
-		slides[0]=slide;
-		slides[1]=slide2;
-		slides[2]=slide;
-		slides[3]=slide2;
-		
-		jump = getToolkit().getImage("./image/jump-0.png");
-		jump2 = getToolkit().getImage("./image/jump-1.png");
-		jump3 = getToolkit().getImage("./image/jump-2.png");
-		jump4 = getToolkit().getImage("./image/jump-3.png");
-		jumps[0]=jump;
-		jumps[1]=jump2;
-		jumps[2]=jump3;
-		jumps[3]=jump4;
-		
+		player_run_images[0]=getToolkit().getImage("./image/run-0.png");
+		player_run_images[1]=getToolkit().getImage("./image/run-1.png");
+		player_run_images[2]=getToolkit().getImage("./image/run-2.png");
+		player_run_images[3]=getToolkit().getImage("./image/run-3.png");
+
+		player_slide_images[0] = getToolkit().getImage("./image/slide-2.png");
+		player_slide_images[1] = getToolkit().getImage("./image/slide-3.png");
+		player_slide_images[2] = player_slide_images[0];
+		player_slide_images[3] = player_slide_images[1];
+
+		player_jump_images[0] = getToolkit().getImage("./image/jump-0.png");
+		player_jump_images[1] = getToolkit().getImage("./image/jump-1.png");
+		player_jump_images[2] = getToolkit().getImage("./image/jump-2.png");
+		player_jump_images[3] = getToolkit().getImage("./image/jump-3.png");
+
 		//������
-		obstacle1 = getToolkit().getImage("./image/obstacle1.png");
-		obstacle2 = getToolkit().getImage("./image/obstacle2.png");
-		obstacle3 = getToolkit().getImage("./image/obstacle3.jpg");
-		fork = getToolkit().getImage("./image/fork.png");
-		
-		heart = getToolkit().getImage("./image/heart.png");
-		heart_eat = getToolkit().getImage("./image/heart_eat.png");
-		heart_long = getToolkit().getImage("./image/heart_long.png");
-		back = getToolkit().getImage("./image/background.png");
-		floor = getToolkit().getImage("./image/floor.png");
-		for(int k=0;k<11;k++) {
-			floors[k]=floor;
+		obstacles[0] = getToolkit().getImage("./image/obstacle1.png");
+		obstacles[1] = getToolkit().getImage("./image/obstacle2.png");
+		obstacles[2] = getToolkit().getImage("./image/obstacle3.jpg");
+		obstacles[3] = getToolkit().getImage("./image/fork.png");
+
+		Image floor = getToolkit().getImage("./image/floor.png");
+		for (int k = 0; k < 11; k++) {
+			floors[k] = floor;
 		}
 		score_font = new Font("Times New Roman", Font.PLAIN, 28);
-		cake = getToolkit().getImage("./image/cake.png");
 		floor_manager = new int[floor_length];
 		cake_manager = new int[floor_length];
 		t1 = new Timer();
 		t1.schedule(new timerTask(),25,25);
 
 		//������
-		Sound.play(0);//background music
+		Sound.play(BACKGROUND);//background music
 		try {
 			InputStream file = getClass().getClassLoader().getResourceAsStream("./floor.txt");
 			Scanner scanner = new Scanner(file);
@@ -121,7 +102,7 @@ public class WalkMian extends JFrame implements KeyListener {
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		
+
 		try {
 			InputStream file = getClass().getClassLoader().getResourceAsStream("./cake.txt");
 			Scanner scanner = new Scanner(file);
@@ -144,19 +125,19 @@ public class WalkMian extends JFrame implements KeyListener {
 				try {
 					while(running) {
 						arun();
-						
+
 						Thread.sleep(100);
-       
+
 					}
-       
+
 				}catch(Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
 		};
 		t.start();
-		
+
 	    setVisible(true);
 	}
 	//���ۿo�B������
@@ -183,11 +164,11 @@ public class WalkMian extends JFrame implements KeyListener {
     		y=500;
     	}
     }
-    
+
     //�����
     public void falling(){
     	if((key_jump == false && y >= 500) && walk_length < -60 && floor_manager[2+walk_floor_length]==0){
-			Sound.play(3);
+			Sound.play(PLAYER_FALLING);
     		y+=90;
 			gameover();
 		}
@@ -205,32 +186,32 @@ public class WalkMian extends JFrame implements KeyListener {
 		if(walk_length < -50) {
 			int use=floor_manager[2+walk_floor_length];
 			if(y > 450 && use==2){
-				Sound.play(6);
+				Sound.play(PLAYER_HIT);
 				heart_value-=10;
 				if(heart_value<=0) {
 					heart_value=0;
 				}
 			}else if(y > 450 && use==3){
-				Sound.play(6);
+				Sound.play(PLAYER_HIT);
 				heart_value-=10;
 				if(heart_value<=0) {
 					heart_value=0;
 				}
 			}else if(y > 350 && use==4){
-				Sound.play(6);
+				Sound.play(PLAYER_HIT);
 				heart_value-=15;
 				if(heart_value<=0) {
 					heart_value=0;
 				}
 			}else if(key_slide == false && use==5){
-				Sound.play(6);
+				Sound.play(PLAYER_HIT);
 				heart_value-=20;
 				if(heart_value<=0) {
 					heart_value=0;
 				}
 			}
 		}
-		
+
 		this.repaint();
 	}
 	//�������B���ۿo�B�����
@@ -246,12 +227,12 @@ public class WalkMian extends JFrame implements KeyListener {
 			repaint();
 		}
 	}
-	
+
 	//�������B���ۿo�B�����
 	public void paint(Graphics g) {
 		super.paint(g);
 		//������
-		g.drawImage(back, 0, 0, this);
+		g.drawImage(backGround, 0, 0, this);
 		g.drawImage(heart_long, 50, 67, heart_value, 21, null);
 		g.drawImage(heart, 25, 50, 50, 50, this);
 		g.setFont(score_font);
@@ -259,17 +240,17 @@ public class WalkMian extends JFrame implements KeyListener {
 		g.drawString("Score:"+Integer.toString(score), 800, 75);
 		//�����
 		if(key_ori==false && key_slide==true){
-			g.drawImage(slides[i], x, y+43, h, w-43, null);
+			g.drawImage(player_slide_images[i], x, y+43, h, w-43, null);
 		}else if(key_ori==true && key_slide==false){
-			g.drawImage(images[i], x, y, h, w, null);
+			g.drawImage(player_run_images[i], x, y, h, w, null);
 		}
 		//���ۿo
 		if(key_ori==false && key_jump==true){
-			g.drawImage(jumps[i], x, y, h, w, null);
+			g.drawImage(player_jump_images[i], x, y, h, w, null);
 		}else if(key_ori==true && key_jump==false){
-			g.drawImage(images[i], x, y, h, w, null);
+			g.drawImage(player_run_images[i], x, y, h, w, null);
 		}
-		
+
 		System.out.println("i"+i);
 		System.out.println("x"+x+"y"+y);
 		paint_floor(g);
@@ -284,15 +265,15 @@ public class WalkMian extends JFrame implements KeyListener {
 				g.drawImage(floors[k], use2, 600, w, h, null);
 			}
 			if(use==2) {
-				g.drawImage(obstacle1, 25+use2, 550, 50, 50, null);
+				g.drawImage(obstacles[0], 25+use2, 550, 50, 50, null);
 			}else if(use==3) {
-				g.drawImage(obstacle2, 25+use2, 550, 50, 50, null);
+				g.drawImage(obstacles[1], 25+use2, 550, 50, 50, null);
 			}else if(use==4) {
-				g.drawImage(obstacle3, 25+use2, 450, 50, 150, null);
+				g.drawImage(obstacles[2], 25+use2, 450, 50, 150, null);
 			}else if(use==5) {
-				g.drawImage(fork, use2, 0, h, 540, null);
+				g.drawImage(obstacles[3], use2, 0, h, 540, null);
 			}
-			
+
 			if(use3==1) {
 				g.drawImage(cake, 25+use2, 550, 30, 30, null);
 			}else if(use3==2) {
@@ -309,38 +290,38 @@ public class WalkMian extends JFrame implements KeyListener {
 		}
 		int use=cake_manager[2+walk_floor_length];
 		if(y > 450 && walk_length < -50 && use==6) {
-			Sound.play(5);
+			Sound.play(EAT_HEART_POTION);
 			heart_value+=100;
 			if(heart_value>300) {
 				heart_value=300;
 			}
 			cake_manager[2+walk_floor_length]=0;
 		}
-		
+
 		if(walk_length < -50) {
 			if(y > 450 && y < 600 && use==1){
-				Sound.play(4);
+				Sound.play(EAT_CAKE);
 				score+=5;
 				cake_manager[2+walk_floor_length]=0;
 			}
 			if(y > 350 && y < 500){
 				if(use==2) {
-					Sound.play(4);
+					Sound.play(EAT_CAKE);
 					score+=5;
 					cake_manager[2+walk_floor_length]=0;
 				}else if(use==3) {
-					Sound.play(4);
+					Sound.play(EAT_CAKE);
 					score+=5;
 					cake_manager[2+walk_floor_length]=0;
 				}
 			}
 			if(y > 250 && y < 450 && use==4){
-				Sound.play(4);
+				Sound.play(EAT_CAKE);
 				score+=5;
 				cake_manager[2+walk_floor_length]=0;
 			}
 			if(y > 480 && y < 650 && use==5){
-				Sound.play(4);
+				Sound.play(EAT_CAKE);
 				score+=5;
 				cake_manager[2+walk_floor_length]=0;
 			}
@@ -349,14 +330,13 @@ public class WalkMian extends JFrame implements KeyListener {
 
 	//�������B���ۿo�B�����
 	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) { 
+		switch (e.getKeyCode()) {
 		case KeyEvent.VK_SPACE :
-			if(key_jump==true){
-				Sound.play(2);
+			if(key_jump){
+				Sound.play(PLAYER_JUMP2);
 				key_jump2=true;
-			}
-			if(key_jump==false){
-				Sound.play(1);
+			} else {
+				Sound.play(PLAYER_JUMP);
 				key_jump=true;
 				key_ori=false;
 			}
@@ -368,9 +348,9 @@ public class WalkMian extends JFrame implements KeyListener {
 		}
 		this.repaint();
 	}
-	
+
 	public void keyReleased(KeyEvent e) {
-		switch (e.getKeyCode()) { 
+		switch (e.getKeyCode()) {
 	    	case KeyEvent.VK_SPACE :
             case KeyEvent.VK_DOWN:
 	    		key_slide = false;
@@ -379,10 +359,10 @@ public class WalkMian extends JFrame implements KeyListener {
 		}
 	}
 	public void keyTyped(KeyEvent e) {
-		
+
 	}
 	//�������B���ۿo�B�����
-	public static void insertTable( int rank,String name,int score) {	
+	public static void insertTable( int rank,String name,int score) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 		} catch (Exception ex) {}
@@ -392,14 +372,14 @@ public class WalkMian extends JFrame implements KeyListener {
 					+ "user=root&password=Olden8874&serverTimezone=UTC&useSSL=false");
 
 			Statement stmt = conn.createStatement();
-		
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO score(Rank,Name,Score) VALUES(?,?,?);"); 
+
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO score(Rank,Name,Score) VALUES(?,?,?);");
 			ps.setLong(1, rank);
-			ps.setString(2, name); 
+			ps.setString(2, name);
 			ps.setLong(3, score);
 			ps.executeUpdate();
 			ResultSet rs1 = stmt.executeQuery("select *" + " from score"+ " where Rank");
-		
+
 			while (rs1.next()) {
 				System.out.println(
 						rs1.getInt(1) + "\t" + rs1.getString(2) + "\t" + rs1.getInt(3) );
@@ -409,7 +389,7 @@ public class WalkMian extends JFrame implements KeyListener {
 			System.out.println("SQLException: " + ex.getMessage());
 			System.out.println("SQLState: " + ex.getSQLState());
 			System.out.println("VendorError: " + ex.getErrorCode());
-			System.out.println("InsertDB Exception :" + ex.toString()); 
+			System.out.println("InsertDB Exception :" + ex.toString());
 		}
 	}
 	//�����
@@ -422,7 +402,7 @@ public class WalkMian extends JFrame implements KeyListener {
 		running = false;
 		input();
 		System.exit(ABORT);
-		
+
 	}
-	
+
 }
